e747ad9fc27f7600a1d2fdca9d5872f5319fa45b
bibique <bibekawal18@gmail.com>
2019-08-14 15:00:55 +0545

    3rd commit

--- meals/admin.py
+++ meals/admin.py
@@ -8,7 +8,7 @@ from .models import Meals , Category
 
 class MealsAdmin(SummernoteModelAdmin , admin.ModelAdmin):  # instead of ModelAdmin
     summernote_fields = '__all__'
-    list_display = ['name' ,'preperation_time', 'people', 'price']
+    list_display = ['name', 'price']
     search_fields = ['name', 'description' ]
     list_filter = ('category','people')
 
